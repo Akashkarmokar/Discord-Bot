38f13fae5fc152ffe82953b262e81db9ffd55b5f
akash <akashcsemu@gmail.com>
2024-01-16 03:30:47 +0600

    fix: Initial Triggering Event By Watcher
    Previously when I changed to the main.py file then the main.py was triggered. Now After start the docker container, Our main file will be triggered.

--- src/watcher.py
+++ src/watcher.py
@@ -1,6 +1,7 @@
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import subprocess
+import os
 
 class MyHandler(FileSystemEventHandler):
     def on_any_event(self, event):
@@ -8,13 +9,20 @@ class MyHandler(FileSystemEventHandler):
             return
         if event.event_type in ('modified', 'created', 'deleted'):
             print(f'Reloading due to {event.event_type} in {event.src_path}')
-            subprocess.run(['python', 'src/main.py'])
+            self.reload_script()
+
+    def reload_script(self):
+        script_path = os.path.join('src', 'main.py')
+        subprocess.run(['python', script_path])
 
 if __name__ == "__main__":
     event_handler = MyHandler()
     observer = Observer()
-    observer.schedule(event_handler, path='.', recursive=True)
+    observer.schedule(event_handler, path='src', recursive=True)  # Adjust the path as needed
     observer.start()
+
+    # Start the main script initially
+    event_handler.reload_script()
 
     try:
         while True:
